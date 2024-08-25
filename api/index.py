bd4174a5d584f7ac5f6a00ec319edd6c74b6d3d5
Lakshya Mittal <lakshyamittal2003@gmail.com>
2024-08-25 13:03:11 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app, resources={r"/bfhl": {"origins": "*"}})
 
 # User information (hardcoded for now)
 user_info = {
